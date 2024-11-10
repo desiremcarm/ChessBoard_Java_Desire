e17d0f1b104b925ec308c6842f1cb5a2b1bd477a
desiremcarm <work@desiremcarmona.com>
2024-11-10 19:41:40 +0100

    Basic approach achieved - still can't print the whole size of each board spot

--- src/MarronDesireEjercicio2.java
+++ src/MarronDesireEjercicio2.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// By Desiré Marrón Carmona 👩🏻
+
+import java.util.Scanner;
+
 public class MarronDesireEjercicio2 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Variables
+
+        // CONST
+        final int BOARD_SIZE = 8;
+        final int MIN_SIZE = 1;
+        final int MAX_SIZE = 15;
+        final int SEPARATOR = 16;
+
+        // User provided
+        char pattern;
+        int size;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Provide your desired pattern. It must contain only 1 character. You can use a number, a symbol, a letter...");
+
+        pattern = scan.next().charAt(0);
+        System.out.println(pattern);
+
+        System.out.println("Provide a desired size for each pattern spot on the board. It must be a value between 1 and 15, both included.");
+        size = scan.nextInt();
+
+        int[][] boardSizeAndPattern = new int[size][size];
+
+        System.out.println(size);
+
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            System.out.println("");
+            for (int j = 0; j < (BOARD_SIZE * size) + 9; j++) {
+                System.out.print("-");
+            }
+            System.out.println("");
+            //  ---------------
+            // [ | | | | | | | ]
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                // [ # |
+                System.out.print("|");
+                for (int k = 0; k < size; k++) {
+                    if(((j+1)%2) == 0){
+                        System.out.print(pattern);
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                if(j + 1 >= BOARD_SIZE){
+                    System.out.print("|");
+                }
+            }
+            if(i + 1 >= BOARD_SIZE){
+                System.out.println("");
+                for (int j = 0; j < (BOARD_SIZE * size) + 9; j++) {
+                    System.out.print("-");
+                }
+            }
         }
+
     }
 }
