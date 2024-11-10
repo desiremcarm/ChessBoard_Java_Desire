3e9a357f37f0e55a2d03d4fff9d10b976fb1437c
desiremcarm <work@desiremcarmona.com>
2024-11-10 22:27:02 +0100

    Fixed cell painting; added demo gif; refactored; now size checks if its a positive integer and between 1 and 15, both included.

--- src/MarronDesireEjercicio2.java
+++ src/MarronDesireEjercicio2.java
@@ -11,52 +11,89 @@ public class MarronDesireEjercicio2 {
         // CONST
         final int BOARD_SIZE = 8;
         final int MIN_SIZE = 1;
-        final int MAX_SIZE = 15;
-        final int SEPARATOR = 16;
-        final int EXTRA_CHARS_PER_ROW = 8;
+        final int MAX_SIZE = 16;
+
+        // CONST COLORS
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_BLUE = "\u001B[34m";
+        final String ANSI_WHITE = "\u001B[37m";
 
         // User provided
         char pattern;
         int size;
 
+        // Control
+        boolean paintEvenSpots = true;
+
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Provide your desired pattern. It must contain only 1 character. You can use a number, a symbol, a letter...");
+        // Asking for a pattern
+        System.out.println(ANSI_BLUE + "Provide your desired pattern. It must contain only 1 character. You can use a number, a symbol, a letter...");
 
+        // Reading pattern
         pattern = scan.next().charAt(0);
-        System.out.println(pattern);
+        System.out.println("The character you've chosen is: " + pattern);
 
-        System.out.println("Provide a desired size for each pattern spot on the board. It must be a value between 1 and 15, both included.");
-        size = scan.nextInt();
+        // Asking for cell size
+        System.out.println("Provide a desired size for each cell on the board. It must be a value between 1 and 15, both included.");
 
-        System.out.println(size);
+        // Checking if given value is a positive integer between 1 and 15
+        do {
+            System.out.print(ANSI_RED + "⚠️ Please, use a positive number between 1 and 15\n");
+            while(!scan.hasNextInt()){
+                System.out.println("⚠️ That value is not valid. Try again");
+                scan.next();
+            }
+            size = scan.nextInt();
+        }while((size >= MIN_SIZE && size >= MAX_SIZE) || size <= 0);
 
+        System.out.println(ANSI_BLUE + "The size you've chosen is: " + size);
+
+        // Main loop for the chess board
         for (int i = 1; i < BOARD_SIZE+1; i++) {
 
             // Upper decoration row
-            //System.out.print("\nstart row: " + i + "\n");
-
             for (int j = 0; j < (BOARD_SIZE * size) + 9; j++) {
-                System.out.print("-");
+                System.out.print(ANSI_WHITE + "-");
             }
+
             System.out.println("");
 
-            for (int h = 0; h < size; h++) { // Paint rows depending on given size
+            for (int h = 0; h < size; h++) {
+
+                // Paint rows depending on given size
                 // Inner row with spots
 
                 for (int k = 1; k < BOARD_SIZE+1; k++) {
-                    // Paint pattern depending if cell is even or not
-                    if((k%2) == 0){
-                        System.out.print("|");
-                        for (int l = 0; l < size; l++) {
-                            System.out.print(pattern);
+
+                    // Logic to paint cells with pattern or empty
+                    if(paintEvenSpots){
+                        if((k+1)%2 == 0){
+                            System.out.print("|");
+                            for (int l = 0; l < size; l++) {
+                                System.out.print(pattern);
+                            }
+                        } else {
+                            System.out.print("|");
+                            for (int l = 0; l < size; l++) {
+                                System.out.print(" ");
+                            }
                         }
                     } else {
-                        System.out.print("|");
-                        for (int l = 0; l < size; l++) {
-                            System.out.print(" ");
+                        if((k+1)%2 == 0){
+                            System.out.print("|");
+                            for (int l = 0; l < size; l++) {
+                                System.out.print(" ");
+                            }
+                        } else {
+                            System.out.print("|");
+                            for (int l = 0; l < size; l++) {
+                                System.out.print(pattern);
+                            }
                         }
                     }
+
+                    // Finish the last cell with "|"
                     if(k+1 >= BOARD_SIZE+1){
                         System.out.print("|");
                     }
@@ -68,55 +105,21 @@ public class MarronDesireEjercicio2 {
                 }
             }
 
+            // Changing the painting cell bool flag
+            if(paintEvenSpots){
+                paintEvenSpots = false;
+            } else {
+                paintEvenSpots = true;
+            }
+
             System.out.println("");
 
+            // End row decoration
             if((i+1) >= BOARD_SIZE+1){
                 for (int j = 0; j < (BOARD_SIZE * size) + 9; j++) {
                     System.out.print("-");
                 }
             }
-
-
         }
-
-        /*
-        for (int i = 0; i < BOARD_SIZE; i++) {
-
-            System.out.println("");
-
-            for (int l = 0; l < (BOARD_SIZE * size) + 8; l++) {
-                System.out.print("-");
-            }
-            System.out.println("");
-
-            // Bucle que pinta casillas del tablero
-            for (int j = 0; j < (BOARD_SIZE * size) + 8; j++) {
-
-                System.out.print("|");
-
-                // Printea el pattern dependiendo de si es casilla par o impar
-                for (int k = 0; k < size; k++) {
-
-                    if(((j+1)%2) == 0){ // Comprueba si es casilla par o impar
-                        System.out.print(pattern);
-                    } else {
-                        System.out.print(" ");
-                    }
-
-                }
-
-                if(((j+1) % 8) == 0){
-                    System.out.print("|");
-                    System.out.println("");
-                }
-
-            }
-            if(i + 1 >= BOARD_SIZE){
-                for (int j = 0; j < (BOARD_SIZE * size) + 8; j++) {
-                    System.out.print("-");
-                }
-            }
-        }
-        */
     }
 }
